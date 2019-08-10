4f61409d7d5a6e0221a062fb1b7813b1d4815964
SHZ <SHZ@HANZHONG>
2019-08-10 22:30:24 +0800

    commit

---
+++ ElasticSearchSample.Console/Employee.cs
@@ -0,0 +1,34 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ElasticSearchSample.Console
+{
+    public class Employee
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime BirthDay { get; set; }
+
+        public string Gender { get; set; }
+
+        public DateTime JoinDate { get; set; }
+
+        public Address Home { get; set; }
+
+        [Text(Fielddata = false)] // mapping中不生成keyword type
+        public string Mobile { get; set; }
+
+        public decimal Salary { get; set; }
+    }
+
+    public class Address
+    {
+        public string Province { get; set; }
+
+        public string City { get; set; }
+    }
+}
---
+++ ElasticSearchSample.Console/EsProvider.cs
@@ -0,0 +1,42 @@
+﻿using Elasticsearch.Net;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ElasticSearchSample.Console
+{
+    public class EsProvider
+    {
+        public EsProvider(string indexName)
+        {
+            var config = new ElasticSearchConfig()
+            {
+                Hosts = new string[] { "http://localhost:9200" },
+                TimeOut = 30
+            };
+
+            var pool = new StaticConnectionPool(config.Hosts.Select(h => new Uri(h)));
+            var settings = new ConnectionSettings(pool)
+                .RequestTimeout(TimeSpan.FromSeconds(config.TimeOut))
+                .DefaultMappingFor<Employee>((descriptor =>
+                {
+                    return descriptor.IndexName("employees");
+                }))
+                .DefaultIndex(indexName);
+            HightClient = new ElasticClient(settings);
+            LowClient = new ElasticLowLevelClient(settings);
+        }
+
+        public ElasticClient HightClient { get; }
+        public ElasticLowLevelClient LowClient { get; }
+    }
+
+    public class ElasticSearchConfig
+    {
+        public string[] Hosts { get; set; }
+
+        public int TimeOut { get; set; }
+    }
+}
